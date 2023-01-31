8a6155a09ce4b657b9707a63e499b7e9be81c3cd
cuimingzhi <18024642@qq.com>
2023-01-31 20:34:28 +0800

    代理类创建

--- Spring/smart-framework/src/main/java/top/sql/framework/proxy/AspectProxy.java
+++ Spring/smart-framework/src/main/java/top/sql/framework/proxy/AspectProxy.java
@@ -22,10 +22,41 @@ public abstract class AspectProxy implements Proxy {
         
         begin();
         
+        try {
+            if (intercept(cls, method, params)) {
+                before(cls, method, params);
+                result = proxyChain.doProxyChain();
+                after(cls, method, params, result);
+            } else {
+                result = proxyChain.doProxyChain();
+            }
+        } catch (Exception e) {
+            logger.error("proxy failure", e);
+            error(cls, method, params, e);
+            throw e;
+        } finally {
+            end();
+        }
         
         return result;
     }
     
     private void begin() {
+    }
+    
+    public boolean intercept(Class<?> cls, Method method, Object[] params) throws Throwable {
+        return true;
+    }
+    
+    public void before(Class<?> cls, Method method, Object[] params) throws Throwable {
+    }
+    
+    public void after(Class<?> cls, Method method, Object[] params, Object result) throws Throwable {
+    }
+    
+    public void error(Class<?> cls, Method method, Object[] params, Throwable e) {
+    }
+    
+    public void end() {
     }
 }
